a04d99f6aabb5013c68888f77d026a0bbe689cd2
catriel458 <catrielcabrera97@gmail.com>
2026-05-03 14:15:51 -0300

    cambiar texto

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -54,8 +54,8 @@ const Services = () => {
     },
     {
       icon: <Wrench className="w-8 h-8" />,
-      title: "Soporte API",
-      description: "Desarrollo y mantenimiento de APIs en PHP para integración de sistemas y aplicaciones.",
+      title: "Desarrollo de Software",
+      description: "Se trabaja en distintos lenguajes como PHP y Phyton para brindar soluciones tecnológicas.",
       image: "https://images.unsplash.com/photo-1558494949-ef010cbdcc31?ixlib=rb-4.0.3&auto=format&fit=crop&w=1000&q=80",
       features: [
         "Desarrollo en PHP",
